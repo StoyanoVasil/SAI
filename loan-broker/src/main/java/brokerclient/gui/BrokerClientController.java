ba84ce45c8904dec8c609d92ba2e9cc6dfe67670
i333656 <vasil.stoyanov@feg.bg>
2018-12-14 10:18:13 +0100

    Move enricher and archiver to BrokerController

--- loan-broker/src/main/java/brokerclient/gui/BrokerClientController.java
+++ loan-broker/src/main/java/brokerclient/gui/BrokerClientController.java
@@ -1,14 +1,18 @@
 package brokerclient.gui;
 
+import brokerclient.messageGateway.Archiver;
 import brokerclient.messageGateway.BankGateway;
 import brokerclient.messageGateway.ClientGateway;
+import brokerclient.messageGateway.CreditHistoryEnricher;
 import brokerclient.model.*;
 import javafx.application.Platform;
 import javafx.scene.control.ListView;
 
 import javax.jms.JMSException;
 import javax.swing.*;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class BrokerClientController {
 
@@ -16,10 +20,22 @@ public class BrokerClientController {
     private ClientGateway clientGateway;
     private BankGateway bankGateway;
 
+    // Declare objects for enriching and archiving
+    private CreditHistoryEnricher enricher;
+    private Archiver archiver;
+
     // javafx objects
     public ListView<ListViewLine> lvBroker;
 
+    // map LoanRequests to BankInterestRequests
+    private Map<BankInterestRequest, LoanRequest> mapLoanToBankInteres;
+
     public BrokerClientController() {
+
+        // initialize enricher and archiver
+        this.enricher = new CreditHistoryEnricher();
+        this.archiver = new Archiver();
+        this.mapLoanToBankInteres = new HashMap<>();
 
         // initialize gateways
         this.clientGateway = new ClientGateway() {
@@ -27,7 +43,10 @@ public class BrokerClientController {
                 try {
                     ListViewLine lvl = new ListViewLine(req);
                     addListViewLineToLv(lvl);
-                    bankGateway.applyForLoan(req);
+                    CreditHistory ch = enricher.getCreditHistoryForSSN(req.getSsn());
+                    BankInterestRequest request = new BankInterestRequest(req.getAmount(), req.getTime(), ch.getCredit(), ch.getHistory());
+                    bankGateway.applyForLoan(request);
+                    mapLoanToBankInteres.put(request, req);
                 } catch (JMSException e) {
                     e.printStackTrace();
                 }
@@ -35,12 +54,16 @@ public class BrokerClientController {
         };
 
         this.bankGateway = new BankGateway() {
-            public void onBankInterestRequestArrived(LoanRequest req, LoanReply rep) {
+            public void onBankInterestRequestArrived(BankInterestRequest req, BankInterestReply rep) {
                 try {
-                    ListViewLine lvl = getLvlForLoanRequest(req);
-                    lvl.setLoanReply(rep);
+                    LoanRequest request = mapLoanToBankInteres.get(req);
+                    ListViewLine lvl = getLvlForLoanRequest(request);
+                    LoanReply reply = new LoanReply(rep.getInterest(), rep.getQuoteId());
+                    lvl.setLoanReply(reply);
                     addListViewLineToLv(lvl);
-                    clientGateway.replyOnRequest(req, rep);
+                    clientGateway.replyOnRequest(request, reply);
+                    archiver.archive(new LoanArchive(request.getSsn(), request.getAmount(),
+                            reply.getQuoteID(), reply.getInterest(), request.getTime()));
                 } catch (JMSException e) {
                     e.printStackTrace();
                 }
--- loan-broker/src/main/java/brokerclient/messageGateway/BankGateway.java
+++ loan-broker/src/main/java/brokerclient/messageGateway/BankGateway.java
@@ -3,40 +3,31 @@ package brokerclient.messageGateway;
 import brokerclient.model.*;
 
 import javax.jms.JMSException;
-import javax.jms.TextMessage;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BankGateway {
 
     private ScatterGather scatterGather;
-    private Map<Integer, LoanRequest> map;
-    private CreditHistoryEnricher enricher;
-    private Archiver archiver;
+    private Map<Integer, BankInterestRequest> map;
+    // TODO: move to controller
 
     public BankGateway() {
 
         this.map = new HashMap<>();
-        this.enricher = new CreditHistoryEnricher();
-        this.archiver = new Archiver();
         this.scatterGather = new ScatterGather() {
             public void onReplyReceived(BankInterestReply reply, Integer aggregationId) {
-                LoanRequest req = map.get(aggregationId);
-                LoanReply rep = new LoanReply(reply.getInterest(), reply.getQuoteId());
-                onBankInterestRequestArrived(req, rep);
-                archiver.archive(new LoanArchive(req.getSsn(), req.getAmount(),
-                        rep.getQuoteID(), reply.getInterest(), req.getTime()));
+                BankInterestRequest req = map.get(aggregationId);
+                onBankInterestRequestArrived(req, reply);
             }
         };
     }
 
-    public void applyForLoan(LoanRequest req) throws JMSException {
+    public void applyForLoan(BankInterestRequest req) throws JMSException {
 
-        CreditHistory ch = this.enricher.getCreditHistoryForSSN(req.getSsn());
-        BankInterestRequest request = new BankInterestRequest(req.getAmount(), req.getTime(), ch.getCredit(), ch.getHistory());
-        int id = this.scatterGather.sendRequest(request);
+        int id = this.scatterGather.sendRequest(req);
         this.map.put(id, req);
     }
 
-    public void onBankInterestRequestArrived(LoanRequest req, LoanReply rep) { }
+    public void onBankInterestRequestArrived(BankInterestRequest req, BankInterestReply rep) { }
 }
